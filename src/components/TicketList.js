76259813f4e764dab61870cceac2c4e3c34e7fe2
Jesse Callahan <jessetylercallahan@gmail.com>
2021-09-07 14:32:02 -0700

    adds git finishing touches

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -4,24 +4,6 @@ import PropTypes from "prop-types";
 import { useSelector } from 'react-redux'
 import { useFirestoreConnect, isLoaded } from 'react-redux-firebase'
 
-// const masterTicketList = [
-//   {
-//     names: 'Thato and Haley',
-//     location: '3A',
-//     issue: 'Firebase won\'t save record. Halp.'
-//   },
-//   {
-//     names: 'Sleater and Kinney',
-//     location: '4B',
-//     issue: 'Prop types are throwing an error.'
-//   },
-//   {
-//     names: 'Imani & Jacob',
-//     location: '9F',
-//     issue: 'Child component isn\'t rendering.'
-//   }
-// ];
-
 function TicketList(props) {
 
   useFirestoreConnect([
